662781d65dab4f928396a274124df1e9716b9bcb
Lakshmi Priyanka Chekuru <priyamanoj@Lakshmis-MacBook-Pro.local>
2021-03-27 23:21:18 -0500

    Create gh-pages branch with static content

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import './App.css';
 import ListFinancialAdvisor from './components/ListFinancialAdvisorComponent';
 import CreateFinancialAdvisor from './components/CreateFinancialAdvisorComponent';
 import UpdateFinancialAdvisor from './components/UpdateFinancialAdvisor';
+import FinancialAdvisorLoginComponent from './components/FinancialAdvisorLoginComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router'
+import RiskToleranceComponent from './components/RiskToleranceComponent';
+import DialogFragmentComponent from './components/DialogFragmentComponent';
+import CustomerInvestmentProfile from './components/CustomerInvestmentProfileComponent';
+import EducationGoalComponent from './components/EducationGoalComponent';
 
 
 function App() {
@@ -20,6 +26,11 @@ function App() {
           <Route path="/online_investment_banking/getFAlist" component={ListFinancialAdvisor}></Route>
           <Route path="/online_investment_banking/createFA" component={CreateFinancialAdvisor}></Route>
           <Route path="/online_investment_banking/getFA/:id" component={UpdateFinancialAdvisor}></Route>
+          <Route path="/online_investment_banking/loginFA" component={FinancialAdvisorLoginComponent}></Route>
+          <Route path="/online_investment_banking/riskTolerance" component={RiskToleranceComponent}></Route>
+          <Route path="/online_investment_banking/goalDialog" component={DialogFragmentComponent}></Route>
+          <Route path="/online_investment_banking/customerInvestProfile" component={CustomerInvestmentProfile}></Route>
+          <Route path="/online_investment_banking/educationGoal" component={EducationGoalComponent}></Route>
       </Switch>
     </div>
     <FooterComponent/>
